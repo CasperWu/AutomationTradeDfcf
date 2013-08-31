0d3d2dcaf156bba870e208401b8d2d101a51a85e
CasperWu <dongqingwan@hotmail.com>
2013-08-31 17:10:42 +0800

    Initial version

---
+++ AutomationTradeDfcf/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationTradeDfcf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dfcf app = new Dfcf();
+            app.StartApp();
+            app.Test1();
+            app.Test2();
+            app.CloseApp();
+        }
+    }
+}
